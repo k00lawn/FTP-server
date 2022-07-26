57648e9c07ebc04c1e5ac63108b71f4a9c86c1b1
k00lawn <k00lawn@gmail.com>
2022-07-26 11:16:51 +0530

    init commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const serveIndex = require("serve-index");
+
+const app = express();
+
+app.use('/ftp', express.static('public/ftp'), serveIndex('public/ftp', {'icons': true}))
+
+app.listen(3000)
