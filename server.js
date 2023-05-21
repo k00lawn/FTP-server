965314469aa1a25a5b54776dfb518dcf57801e05
Koolawn <k00lawn@gmail.com>
2023-05-21 11:26:30 +0530

    final changes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const serveIndex = require("serve-index");
+const PORT = process.env.PORT;
+
 
 const app = express();
 
+if (!PORT) {
+    throw new Error("PORT variable not defined");
+}
+
+app.get("/home", (req, res) => {
+    const data = `App running at PORT ${PORT}`;
+    return res.send(data);
+});
+
 app.use('/ftp', express.static('public/ftp'), serveIndex('public/ftp', {'icons': true}))
 
-app.listen(3000)
+app.listen(PORT, "0.0.0.0", () => console.log(`Server at ${PORT}`));
+
